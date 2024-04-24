02a167bd5d7e0780a9f5c27effe34594ad65a2d9
Glen Souza <glenster75@gmail.com>
2024-04-24 16:34:27 -0700

    Update NavMenu to include Votes and Results links

---
+++ FTB.BlazorWASM/Models/VoteViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FTB.BlazorWASM.Models;
+
+public class VoteViewModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string Car1Name { get; set; } = string.Empty;
+    public string Car2Name { get; set; } = string.Empty;
+    public string? Winner { get; set; }
+    public double Score { get; set; } = 0;
+}
