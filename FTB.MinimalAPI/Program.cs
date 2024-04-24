a999d01c50e47f6f7d2f04f6e4dc029f5ba4f20e
Glen Souza <glenster75@gmail.com>
2024-04-24 14:21:21 -0700

    Update NavMenu to include Random Car link and update page titles

--- FTB.MinimalAPI/Program.cs
+++ FTB.MinimalAPI/Program.cs
@@ -4,7 +4,15 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ThisAutomobileDoesNotExistService>();
 
+builder.Services.AddCors();
+
 WebApplication app = builder.Build();
+
+app.UseCors(appBuilder => appBuilder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
 
 app.MapPost("/api/CalculateElo/{winnerRating:double}/{loserRating:double}", EloCalculator.CalculateElo);
 
