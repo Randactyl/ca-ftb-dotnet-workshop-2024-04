f6a9ce23a23f88ae2fc9d4f4554b6e1dd9a4aff4
Glen Souza <glenster75@gmail.com>
2024-04-24 14:05:07 -0700

    Add HtmlAgilityPack package reference and update FTB.Common.csproj

---
+++ FTB.Common/ThisAutomobileDoesNotExistService.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+
+namespace FTB.Common;
+
+public class ThisAutomobileDoesNotExistService
+{
+    private const string ServiceUrl = "https://www.thisautomobiledoesnotexist.com/";
+
+    private readonly HttpClient httpClient = new();
+
+    private readonly HtmlDocument document = new();
+
+    public async Task<string> GetRandomAutomobileImageAsync()
+    {
+        string automobileDoesNotExistHtml = await this.httpClient.GetStringAsync(ServiceUrl);
+        if (string.IsNullOrEmpty(automobileDoesNotExistHtml))
+        {
+            throw new Exception("website down!");
+        }
+
+        this.document.LoadHtml(automobileDoesNotExistHtml);
+
+        // Check if the image exists
+        HtmlNode imgNode = this.document.DocumentNode.SelectSingleNode("//img[@id='vehicle']") ?? throw new Exception("website down!");
+        string src = imgNode.GetAttributeValue("src", "");
+        return src;
+    }
+}
--- FTB.MinimalAPI/Program.cs
+++ FTB.MinimalAPI/Program.cs
@@ -1,22 +1,13 @@
 using FTB.Common;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ThisAutomobileDoesNotExistService>();
+
 WebApplication app = builder.Build();
 
-app.MapPost("/api/CalculateElo/{winnerRating}/{loserRating}", (double winnerRating, double loserRating) =>
-{
-    Console.WriteLine($"Winner rating before Elo calculation: {winnerRating}");
-    Console.WriteLine($"Loser rating before Elo calculation: {loserRating}");
+app.MapPost("/api/CalculateElo/{winnerRating:double}/{loserRating:double}", EloCalculator.CalculateElo);
 
-    EloCalculationModel elo = EloCalculator.CalculateElo(winnerRating, loserRating);
-
-    Console.WriteLine();
-    Console.WriteLine($"New winner rating: {winnerRating + elo.WinnerScore}");
-    Console.WriteLine($"New loser rating: {loserRating + elo.LoserScore}");
-
-    return elo;
-});
-
-app.MapPost("/api/CalculateElo2/{winnerRating:double}/{loserRating:double}", EloCalculator.CalculateElo);
+app.MapGet("/api/GetCarPicture", (ThisAutomobileDoesNotExistService service) => service.GetRandomAutomobileImageAsync());
 
 app.Run();
