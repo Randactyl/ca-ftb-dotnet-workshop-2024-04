fb967e0fe4a9e142d539db793ea80102dcc420f1
Randactyl <rafowler@randactyl.com>
2024-04-26 11:46:46 -0700

    improved home, game, and scoreboard pages

---
+++ FTB.TriviaWeb/Models/InProgressGame.cs
@@ -0,0 +1,9 @@
+﻿using FTB.TriviaLib;
+
+namespace FTB.TriviaWeb.Models;
+
+public class InProgressGame
+{
+    public GameSettingsModel Settings { get; set; } = new();
+    public OpenTriviaModel OpenTriviaModel { get; set; } = new();
+}
